63fe684c6a1808c4dd3fb3acf8c86ab6578855cf
Edu <eduardo.rc02@gmail.com>
2018-02-14 21:51:10 -0200

    teste aula 04

--- logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit.java
+++ logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit.java
@@ -2,9 +2,6 @@ package br.com.malhaLogistica;
 
 public class TesteGit {
 	
-	public static void main(String[] args) {
-		System.out.println("Eduardo");
-		System.out.println("Eduardo");
-		}
+ 
 
 }
