6ad9a592207d1818eadb9a48eca63f11cd8d6506
Edu <eduardo.rc02@gmail.com>
2018-02-14 22:09:06 -0200

    commit teste

--- logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit.java
+++
@@ -1,7 +0,0 @@
-package br.com.malhaLogistica;
-
-public class TesteGit {
-	
- 
-
-}
--- logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit03.java
+++ logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit03.java
@@ -3,6 +3,7 @@ package br.com.malhaLogistica;
 public class TesteGit03 {
 	
 	public static void main(String[] args) {
+		System.out.println("Teste branch");
 		
 	}
 
