95ba6e50cbe3d451c093de84283af99e0f3357c5
Edu <eduardo.rc02@gmail.com>
2018-02-14 22:16:24 -0200

    nova funcionalidade testada

--- logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit03.java
+++ logisticNetwork/src/test/java/br/com/malhaLogistica/TesteGit03.java
@@ -3,8 +3,14 @@ package br.com.malhaLogistica;
 public class TesteGit03 {
 	
 	public static void main(String[] args) {
+		
+		boolean eqTrue = true;
+		
 		System.out.println("Teste branch");
 		
+		if(eqTrue){
+			System.out.println("True");
+		}
 	}
 
 }
